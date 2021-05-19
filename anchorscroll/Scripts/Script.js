db2accfcf161983c1240b99241f60a936d962174
UJustBe <UJustBe@DESKTOP-FRVLAGQ>
2021-05-19 13:33:51 +0530

    Add project files.

---
+++ anchorscroll/Scripts/Script.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.js" />
+
+var demoapp = angular.module("DemoApp", []);
+
+demoapp.controller("MyController", function ($scope, $location, $anchorScroll) {
+                          $scope.scrollTo = function (scrollLocation) {
+                          $location.hash(scrollLocation);
+                         $anchorScroll();
+
+                           }
+                       });
